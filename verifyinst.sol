e65cfa686084bc9251237486ce551b3769a64c4e
Aureum21 <120782786+Aureum21@users.noreply.github.com>
2025-06-19 11:30:45 +0400

    Update verifyinst.sol

--- verifyinst.sol
+++ verifyinst.sol
@@ -2,24 +2,23 @@
 
 pragma solidity ^0.8.0;
 
-import "https://github.com/Aureum21/blockchain/blob/main/registerInst.sol";
-import "https://github.com/Aureum21/blockchain/blob/main/institute.sol";
+import "./registerInst.sol";
+import "./institute.sol";
 
 contract verifyinst {
     address public MOE;
     registerInst public instreg;
-
     constructor(address _registerInstAddress) {
         MOE = msg.sender;
         instreg = registerInst(_registerInstAddress);
     }
-
     modifier onlyMOE() {
         require(msg.sender == MOE);
         _;
     }
-
+    string[] public institution_names;
     mapping(address => address) public registeredinstitutesmap;
+    mapping(string =>address) public instNameToAddress;
     address[] registeredinstitutes;
 
     function verifyInstitution(bytes32 key, uint256 index) public onlyMOE {
@@ -38,6 +37,19 @@ contract verifyinst {
         registeredinstitutesmap[institute_address] = address(newinst);
         registeredinstitutes.push(institute_address);
         instreg.removePendingInstitute(key, index);
+        instNameToAddress[institute_name]=institute_address;
+        institution_names.push(institute_name);
+    }
+    function getInstitutesCount() public view returns (uint256) {
+        return institution_names.length;
+    }
+    function getAddresByName(string memory name) public view returns (address){
+        return instNameToAddress[name];
+    }
+    function getInstituteByIndex(
+        uint256 index
+    ) public view returns (string memory) {
+        return institution_names[index];
     }
 
     function getPendingInstitutesCount() public view returns (uint256) {
