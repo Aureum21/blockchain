65ee9dd1a8eb47a85e540faa603e9b232628e2e7
Aureum21 <120782786+Aureum21@users.noreply.github.com>
2025-06-19 11:29:47 +0400

    Update verifystud.sol

--- verifystud.sol
+++ verifystud.sol
@@ -2,14 +2,16 @@
 
 pragma solidity ^0.8.0;
 
-import {registerStud} from "https://github.com/Aureum21/blockchain/blob/main/registerStud.sol";
-import {verifyinst} from "https://github.com/Aureum21/blockchain/blob/main/verifyinst.sol";
+import {registerStud} from "./registerStud.sol";
+import {student} from "./student.sol";
+import {verifyinst} from "./verifyinst.sol";
 
 contract verifystud {
     address public MOE;
     registerStud public studreg;
     verifyinst public instregd;
     address public studentRegisterContractAddress; //to be removed cause repeated
+    uint256 public trialnumber = 0;
 
     constructor(address _registerStudAddress, address _registeredInstAddress) {
         MOE = msg.sender;
@@ -44,7 +46,7 @@ contract verifystud {
         return keccak256(abi.encodePacked(_address, _id));
     }
 
-    function addStudToPending(string memory _name, address _student_address, string memory _email, string memory _id, address _instAddress) public onlystudentorMOE{
+    function addStudToPending(string memory _name, address _student_address, string memory _email, string memory _id, address _instAddress) public onlystudent{
         require(msg.sender == _student_address);
         pendingstudents memory newpendingstudent;
         newpendingstudent.name = _name;
@@ -68,7 +70,7 @@ contract verifystud {
             _student_address,
             _id
         );
-        
+       
         (,,,,address currentInstAddress)=studreg.getRegisteredStudentByKey(key);
         require(msg.sender == currentInstAddress);
         pendingstudents memory newpendingstudent;
@@ -81,49 +83,52 @@ contract verifystud {
     }
 
     function verifystudent(uint256 index) public onlyverfiedInstitute  {
-        (, address student_address,,string memory id,address instaddress)=getPendingStudentByindex(index);
-        require(msg.sender == instaddress, "Not the Student's Institute");
-        
+        (string memory name,address student_address,string memory email,string memory id,address instaddress)=getPendingStudentByindex(index);
+        require(msg.sender == instaddress, "Not the Stuent's Institute");
+       
         // address studContractaddress = studreg.getRegisteredStudentsContract(
         //     student_address
         // );
         // require(studContractaddress != address(0), "Invalid student contract address(Studetnt not Registerd))");
-        
+       
         bytes32 key = studreg.generateKey(
             student_address,
             id
         );
-        require(studreg.addInstTostudInfo(key, msg.sender), "Failed to add institute to student info");
+        require(studreg.addInstTostudInfo(index, key, msg.sender), "Failed to add institute to student info");
+        delete pendingStudentsarray[index];
         // student(studContractaddress).addInstToProfile(msg.sender);
 
     }
 
      function verifyTransferstudent(uint256 index) public {
-        (,address student_address,,string memory id,address instaddress)=getTransferStudentByindex(index);
-        require(msg.sender == instaddress, "Not the Student's Institute");
-        
+        (string memory name,address student_address,string memory email,string memory id,address instaddress)=getTransferStudentByindex(index);
+        require(msg.sender == instaddress, "Not the Stuent's Institute");
+       
         // address studContractaddress = studreg.getRegisteredStudentsContract(
         //     student_address
         // );
         // require(studContractaddress != address(0), "Invalid student contract address(Studetnt not Registerd))");
-        
+       
         bytes32 key = studreg.generateKey(
             student_address,
             id
         );
-        require(studreg.addInstTostudInfo(key, msg.sender), "Failed to add institute to student info");
+        require(studreg.addInstTostudInfo(index, key, msg.sender), "Failed to add institute to student info");
+        delete transferStudentsarray[index];
+
         // student(studContractaddress).addInstToProfile(msg.sender);
 
     }
-    function removeGraduateStud(address student_address,string memory id) public {
+    function removeGraduateStud(uint256 index,address student_address,string memory id) public {
         bytes32 key = studreg.generateKey(
             student_address,
             id
         );
-        
-        (,address studAddress,,,address currentInstAddress)=studreg.getRegisteredStudentByKey(key);
-        require((msg.sender == currentInstAddress) || (msg.sender == studAddress));
-        require(studreg.removeInstFromstudInfo(key), "Failed to remove institute from student info");
+       
+        (,,,,address currentInstAddress)=studreg.getRegisteredStudentByKey(key);
+        require(msg.sender == currentInstAddress);
+        require(studreg.removeInstFromstudInfo(index, key), "Failed to remove institute from student info");
     }
 
     function getTransferStudentsCount() public view returns (uint256) {
@@ -140,7 +145,9 @@ contract verifystud {
     )
         public
         view
-        returns (string memory, address, string memory, string memory, address)
+        returns (string memory, address,
+       
+        string memory, string memory, address)
     {
         require(index < pendingStudentsarray.length, "Index out of bounds");
 
@@ -162,7 +169,7 @@ contract verifystud {
         view
         returns (string memory, address, string memory, string memory, address)
     {
-        require(index < transferStudentsarray.length, "Index out of bounds");
+        require(index < pendingStudentsarray.length, "Index out of bounds");
 
         pendingstudents memory studentinfo = transferStudentsarray[index];
 
@@ -179,7 +186,7 @@ contract verifystud {
         return registeredStudentsmap[_studentAddress] != address(0x0);
     }
 
-    
+   
 
     function StudentsCount() public view returns (uint256) {
         return registeredStudents.length;
@@ -196,12 +203,12 @@ contract verifystud {
     }
 
     modifier onlyverfiedInstitute() {
-        
+
         require(instregd.registeredinstitutesmap(msg.sender)!=address(0), "You are not Authorized!");
         _;
     }
-    modifier onlystudentorMOE () {
-        require(studreg.isStudent(msg.sender) || (msg.sender == MOE), "Not authorized");
+    modifier onlystudent () {
+        require(studreg.isStudent(msg.sender), "Not authorized");
         _;
     }
 }
